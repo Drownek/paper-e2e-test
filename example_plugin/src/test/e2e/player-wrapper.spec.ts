332e40dd6856fd41dd0234ba359300d10f9a7645
Piotr <piotrgamingyt@gmail.com>
2026-02-16 19:08:01 +0100

    test: add player-wrapper.spec.ts
    Cover makeOp, deOp, chat, setGameMode, teleport, giveItem

---
+++ example_plugin/src/test/e2e/player-wrapper.spec.ts
@@ -0,0 +1,32 @@
+import { expect, sleep, test } from '@drownek/paper-e2e-runner';
+
+test('makeOp', async ({ player }) => {
+    await player.makeOp();
+    await expect(player).toHaveReceivedMessage(`Made ${player.username} a server operator`)
+});
+
+test('deOp', async ({ player }) => {
+    await player.deOp();
+    await player.chat('/op');
+    await expect(player).toHaveReceivedMessage('Unknown command');
+});
+
+test('chat', async ({ player }) => {
+    await player.chat('/help');
+    await expect(player).toHaveReceivedMessage('Help');
+});
+
+test('gamemode', async ({ player }) => {
+    await player.setGameMode('adventure');
+    expect(player.bot.game.gameMode).toBe('adventure');
+});
+
+test('teleport', async ({ player }) => {
+    await player.teleport(123, 100, 321)
+    expect(player.bot.entity.position).toMatchObject({ x: 123.5, z: 321.5 });
+});
+
+test('giveItem', async ({ player }) => {
+    await player.giveItem('emerald', 5)
+    await expect(player).toContainItem('emerald', 5)
+});
